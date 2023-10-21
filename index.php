17bd3c0c8f5e801fe0d336c7f4c2a6f4e2dcad4d
devpropay <pay69works@gmail.com>
2023-10-21 11:01:03 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
              <h1>Hello World</h1>
              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, ipsum quibusdam dicta ullam eveniet hic unde, saepe consequuntur placeat non praesentium quisquam. Aliquam deserunt illum a nesciunt voluptates, ab reprehenderit!</p>
+             <ul>
+                          <li>satu</li>
+                          <li>dua</li>
+             </ul>
 </body>
 </html>
