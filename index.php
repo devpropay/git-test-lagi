95c5559fdf116381329c7d57c072242bca5d78c4
devpropay <pay69works@gmail.com>
2023-10-21 10:03:33 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
              <h1>Hello World</h1>
+             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, ipsum quibusdam dicta ullam eveniet hic unde, saepe consequuntur placeat non praesentium quisquam. Aliquam deserunt illum a nesciunt voluptates, ab reprehenderit!</p>
 </body>
 </html>
